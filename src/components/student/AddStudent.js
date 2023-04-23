6f3f896e56b89295f5739db8f6a9e95d7976b616
Raphael Eze <eze.raph@gmail.com>
2023-04-23 22:59:38 +0100

    fixed unneccesary import bug

--- src/components/student/AddStudent.js
+++ src/components/student/AddStudent.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { Link } from "react-router-dom";
 
 function AddStudent() {
   const [allCourses, setAllCourses] = useState([]);
